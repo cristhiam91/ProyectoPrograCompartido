8fdc51b2325c9667e6e2781866d133329029cf65
VALE\Ingrid Rivera <ivrc_s_95@hotmail.com>
2018-08-07 07:06:10 -0600

    PM&Boss

---
+++ PmTool/PmTool.UI/Models/MyPMProjects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PmTool.UI.Models
+{
+    public class MyPMProjects
+    {
+        public List<Factories> Factories { get; set; }
+        public List<DataCenters> DataCenters { get; set; }
+        public List<Labs> Labs { get; set; }
+        public List<Offices> Offices { get; set; }
+        public List<OtherProjects> OtherProjects { get; set; }
+    }
+}
